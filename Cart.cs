f30b670f85832e4fdcf781dd58fb9c21b7938e61
Cedric Karns-Waters <nkarnswaters@gmail.com>
2022-07-13 12:02:19 -0400

    Added Book and Cart models

---
+++ Cart.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace backend
+{
+    public class Cart
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public Book[] books { get; set; }
+    }
+}
