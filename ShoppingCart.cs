bc302c3058a36645fd6853b659ca50f763a2ecd9
Cedric Karns-Waters <nkarnswaters@gmail.com>
2022-07-13 11:41:38 -0400

    Create model for Shopping Cart

---
+++ ShoppingCart.cs
@@ -0,0 +1,11 @@
+﻿namespace backend
+{
+    public class ShoppingCart
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Qty { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal LinePrice { get { return UnitPrice * Qty; } }
+    }
+}
